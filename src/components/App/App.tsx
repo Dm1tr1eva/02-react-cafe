e294e8af588b24d2eb0194ab53bc0583b15a70fb
Dm1tr1eva <114660725+Dm1tr1eva@users.noreply.github.com>
2026-04-18 09:05:15 +0200

    add some code

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+
+import CafeInfo from '../CafeInfo/CafeInfo';
+import css from './App.module.css'
+// import clsx from 'clsx';
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo/>
+    </div>
+  )
+}
